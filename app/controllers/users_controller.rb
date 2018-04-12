588e482fe499d02712fbcfdd62d0fc1c4ff2c8d3
Berry  Semexan <bsemexan@gmail.com>
2018-04-12 05:31:29 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
      session[:user_id] = user.id
      redirect_to root_path
    else
-     redirect_to
+     redirect_to new_user_path
  end
 
  private
