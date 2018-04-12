18a62c36dadba7045165bf0ea1e6d1ebba0bde0e
Berry  Semexan <bsemexan@gmail.com>
2018-04-12 04:47:59 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,9 @@
 class SessionsController < ApplicationController
   def new
   end
-  
+
   def create
-    @user = User.find_by(name: params[:username])
+    @user = User.find_by(name: params[:user][:name])
     return head(:forbidden) unless @user.authenticate(params[:password])
     session[:user_id] = @user.id
   end
